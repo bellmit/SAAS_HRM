b6db771ae2d41e9e15de3456738f9d309770a05b
lcv8 <1242117252@qq.com>
2021-03-22 19:49:55 +0800

    自定义异常类

---
+++ ihrm_common/src/main/java/com/JPP/common/exception/CommonException.java
@@ -0,0 +1,20 @@
+package com.JPP.common.exception;
+
+import com.JPP.common.entity.Result;
+import com.JPP.common.entity.ResultCode;
+import lombok.Getter;
+
+@Getter
+public class CommonException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    private ResultCode code = ResultCode.SERVER_ERROR;
+
+    public CommonException() {
+    }
+
+    public CommonException(ResultCode code) {
+        super(ResultCode.FAIL.message());
+        this.code = code;
+    }
+}
